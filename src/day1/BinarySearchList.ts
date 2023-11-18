af56748286406c734b653b80212ad0364de0b41d
bazen <bazenatlas@gmail.com>
2023-11-18 11:05:20 -0500

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let l = 0;
+    let r = haystack.length - 1;
 
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (haystack[m] === needle) return true;
+        else if (haystack[m] > needle) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+    return false;
 }
