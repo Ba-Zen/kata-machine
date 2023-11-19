ac76aab4b5a19c798681d9c94c78828915bba80a
bazen <bazenatlas@gmail.com>
2023-11-19 16:32:09 -0500

    two crystals

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -4,6 +4,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
 
     while (l <= r) {
         let m = Math.floor((l + r) / 2);
+
         if (haystack[m] === needle) return true;
         else if (haystack[m] > needle) {
             r = m - 1;
@@ -13,3 +14,19 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     }
     return false;
 }
+// export default function bs_list(haystack: number[], needle: number): boolean {
+//     let l = 0;
+//     let r = haystack.length - 1;
+
+//     while (l <= r) {
+//         let m = Math.floor((l + r) / 2);
+
+//         if (haystack[m] === needle) return true;
+//         else if (haystack[m] > needle) {
+//             r = m - 1;
+//         } else {
+//             l = m + 1;
+//         }
+//     }
+//     return false;
+// }
